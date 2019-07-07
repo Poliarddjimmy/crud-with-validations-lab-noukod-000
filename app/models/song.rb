d61d6f095d8bbde67954bb09fb942e6a2e83f87b
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-07 05:42:32 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,6 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: { in: [true, false] }
   validates :release_year, presence: true, numericality: { less_than_or_equal_to: Time.new.year }, if: :released
   validates :artist_name, presence: true
+
+  
 end
